e1fb9cfd497daeceb414e1456dbbd72ab56ac6be
limenghua <android.hua@gmail.com>
2017-02-07 14:28:37 +0800

    modify comment

--- my script/animals.py
+++ my script/animals.py
@@ -28,7 +28,7 @@ animal = Animal()
 animal.run_twice(cat)
 animal.run_twice(dog)
 
-print('Timer没有继承Animal类，但是有run方法，也可以被传递给animal参数')
+print('Timer没有继承Animal类，但是有run方法，也可以被传递给x参数')
 # 这就是动态语言的“鸭子类型”，它并不要求严格的继承体系，一个对象只要“看起来像鸭子，走起路来像鸭子”，那它就可以被看做是鸭子
 class Timer(object):
 	def run(self):
