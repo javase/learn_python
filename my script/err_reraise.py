3e12643053169e0dd8c9d68dd6c096e7d5c7e8d9
limenghua <android.hua@gmail.com>
2017-02-08 16:41:25 +0800

    add err handle

---
+++ my script/err_reraise.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+def foo(s):
+	n = int(s)
+	if n==0:
+		raise ValueError('invalid value:%s' %s)
+	return 10/n
+
+def bar():
+	try:
+		foo('0')
+	except ValueError as e:
+		print('ValueError!')
+		# 注意tab层次	
+		raise
+			
+bar()
