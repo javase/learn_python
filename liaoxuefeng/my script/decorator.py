4626618a06d32a4c85cf943a5cdab7aee190d56c
limenghua <limenghua@wondersgroup.com>
2017-06-15 09:29:37 +0800

    装饰器

--- liaoxuefeng/my script/decorator.py
+++ liaoxuefeng/my script/decorator.py
@@ -1,35 +1,48 @@
 # -*- coding: utf-8 -*-
 def now():
-	print('2017.01.16')
-	
+    print('2017.01.16')
+
+
 f = now
-print('调用函数 f():' ,f())	
-print('函数的名字，注意下划线是两根，f.__name__：',f.__name__)
+print('调用函数 f():', f())
+print('函数的名字，注意下划线是两根，f.__name__：', f.__name__)
+
 
 def log(func):
-	def wrapper(*args,**kw):
-		print('call %s():' %func.__name__)
-		return func(*args,**kw)
-	return wrapper
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
 
-print('\n\n---------------------------------')	
-@log	
+    return wrapper
+
+
+print('\n\n---------------------------------')
+
+
+@log
 def now2():
-	print('2017年1月16日 16:51:02')	
-	
-now2()	
+    print('2017年1月16日 16:51:02')
 
-print('\n\n---------------------------------')	
+
+now2()
+
+print('\n\n---------------------------------')
+
+
 def log2(text):
-	def decorator(func):
-		def wrapper(*args,**kw):
-			print('%s %s():' %(text,func.__name__))
-			return func(*args,**kw)
-		return wrapper
-	return decorator
-	
-@log2('excute')	
-def now3():	
-	print('2017年1月16日 16:53:32')
-	
-now3()	
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+
+        return wrapper
+
+    return decorator
+
+
+@log2('excute')
+def now3():
+    print('2017年1月16日 16:53:32')
+
+
+now3()
--- liaoxuefeng/my script/decorator_wrapper.py
+++ liaoxuefeng/my script/decorator_wrapper.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 import functools
 
+
 def log(func):
-	@functools.wraps(func)
-	def wrapper(*args,**kw):
-		print('call %s():' %func.__name__)
-		return func(*args,**kw)
-	return wrapper
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+
+    return wrapper
+
 
 @log
 def now():
-	print('2017年1月16日 16:56:54')
-		
-now()		
+    print('2017年1月16日 16:56:54')
+
+
+now()
